ab02ac19b0dd567ba921fc398b222d70b2689923
AliFeyzi78 <zeron1g2k@gmail.com>
2020-11-05 11:55:01 +0200

    Fetching with then-catch

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, } from 'react-native';
+
+
+const Main = (props) => {
+
+  const fetchData_Then = () => {
+    let myData = []
+
+    console.log("starting fetch...")
+
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        console.log(response);
+        myData = response.data;
+      })
+    console.log(myData);
+    console.log("end fetch....")
+
+  }
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Main</Text>
+        <Button title="Fetch Data" onPress={fetchData_Then} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default Main;
